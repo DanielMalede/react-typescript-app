1b4c879fe28156bd3b0bbd26b90d1dac39b753cd
DanielMalede <danielmalede@gmail.com>
2022-09-15 16:35:58 +0300

    .

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,16 +1,21 @@
+import { getUser } from "../../../services/user.service";
 import "./Home.css";
-// import Iuser from "../../../models/userModel";
+import Iuser from "../../../models/userModel";
 function Home() {
-  const printHome =(num1:number,num2:number):string => {
-    return "Hello World"
-  }
-  // const newUser:Iuser  
+  const printHome = (num1: number, num2: number): string => {
+    return "Hello World";
+  };
+  getUser().then((users:Iuser[])=>{
+    for (const key in users) {
+      
+    }
+  }) 
   return (
     <div className="home">
       <h1>Home</h1>
-      {printHome(1,2)}
+      {printHome(1, 2)}
     </div>
   );
-};
+}
 
 export default Home;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,14 @@
 import Iuser from "../models/userModel";
 const API: string =
   "https://my-json-server.typicode.com/Jeck99/fake-server/users";
-export const getUser = async () => {
+export const getUser = async (): Promise<any> => {
   try {
     return await fetch(API)
-      .then((res) => res.json())
-      .then((res) => console.log(res));
+      .then((res: Response) => res.json())
+      .then((result) => {
+        let usersArray:Iuser[] = result
+        return usersArray
+      });
   } catch (err) {
     console.log(err);
   }
